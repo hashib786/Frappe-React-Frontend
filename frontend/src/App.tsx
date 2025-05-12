a3ea6654f15b5bd6049baf432b0d9ff056f4365a
Hashib Raja <hashibraja23@gmail.com>
2025-05-12 03:10:23 +0000

    feat: update App component to include Hashib in the title and remove Vite logo feat: enhance Testing Doctype by adding common_name and about fields

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,14 +18,11 @@ function App() {
       <div>
         <h1>{userName}</h1>
         <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
           <a href="https://reactjs.org" target="_blank">
             <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
-        <h1>Vite + React + Frappe</h1>
+        <h1>Vite + React + Frappe + Hashib</h1>
         <div className="card">
           <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
