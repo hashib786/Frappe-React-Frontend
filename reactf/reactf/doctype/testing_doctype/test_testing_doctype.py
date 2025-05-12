85f53a70121d54859828c2923d4f9bc2f7065735
Hashib Raja <hashibraja23@gmail.com>
2025-05-12 01:46:15 +0000

    feat: add Testing Doctype with initial structure and test cases

---
+++ reactf/reactf/doctype/testing_doctype/test_testing_doctype.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Hashib Raja and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTestingDoctype(FrappeTestCase):
+	pass
