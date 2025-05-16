6ed65b858a6d2400172b979202edc98d8ea2be39
Hashib Raja <hashibraja23@gmail.com>
2025-05-16 00:17:19 +0000

    feat: add Rental Items and Ride Booking DocTypes with initial implementations

---
+++ reactf/rentals/doctype/ride_booking/ride_booking.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Hashib Raja and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Ride Booking", {
+// 	refresh(frm) {
+
+// 	},
+// });
