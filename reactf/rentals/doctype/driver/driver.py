adadb00410ddf19a57a2a87e9b5522da892a3db6
Hashib Raja <hashibraja23@gmail.com>
2025-05-16 01:37:46 +0000

    feat: enhance Driver DocType with new fields and templates, and implement Ride Booking button functionality

--- reactf/rentals/doctype/driver/driver.py
+++ reactf/rentals/doctype/driver/driver.py
@@ -2,10 +2,10 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class Driver(Document):
+class Driver(WebsiteGenerator):
     def before_save(self):
         print(self)
         # set the full name of the driver
--- reactf/rentals/doctype/ride_booking/ride_booking.js
+++ reactf/rentals/doctype/ride_booking/ride_booking.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2025, Hashib Raja and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Ride Booking", {
-// 	refresh(frm) {
+frappe.ui.form.on("Ride Booking", {
+    refresh(cur_frm) {
+        cur_frm.add_custom_button(__("Accept"), function () {
+            frappe.show_alert({
+                message: __("Ride Booking Accepted"),
+                indicator: "green",
+            })
+        });
 
-// 	},
-// });
+    },
+});
