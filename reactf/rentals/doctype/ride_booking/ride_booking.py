9ec5cd84450b4889b4c467c6aa69ac960fba7750
Hashib Raja <hashibraja23@gmail.com>
2025-05-16 00:44:02 +0000

    feat: update Ride Booking DocType to include rate and total amount calculations, and modify Rental Items field type

--- reactf/rentals/doctype/ride_booking/ride_booking.py
+++ reactf/rentals/doctype/ride_booking/ride_booking.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2025, Hashib Raja and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class RideBooking(Document):
-	pass
+    def validate(self):
+        if not self.rate:
+            frappe.throw("Rate is required")
+        # set the total fare
+        items = self.items
+        total_distance = 0
+        for item in items:
+            total_distance += item.destination
+        self.total_amount = total_distance * self.rate
